275df74c020bb6987e3332786ebfc9776d6ea058
htpan610 <htpan1991@163.com>
2019-09-05 23:57:53 +0800

    2019年9月5日23:56:51更新

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post,Category
+from .models import Post, Category
 import markdown
+from comments.forms import CommentForm
 # Create your views here.
 from django.http import HttpResponse
 
@@ -12,15 +13,26 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.body = markdown.markdown(post.body, extensions=[ 'extra','codehilite','toc',])
-    return render(request, "blog/detail.html", context={"post": post})
+    post.body = markdown.markdown(post.body, extensions=['extra', 'codehilite', 'toc', ])
+
+    form = CommentForm()
+
+    comment_list = post.comment_set.all()
+
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
+
 
 def archives(request, year, month):
-    post_list=Post.objects.filter(created_time__year=year,
-                                  created_time__month=month).order_by("-created_time")
-    return render(request,"blog/index.html",context={"post_list":post_list})
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month).order_by("-created_time")
+    return render(request, "blog/index.html", context={"post_list": post_list})
 
-def category(request,pk):
-    cate=get_object_or_404(Category,pk=pk)
-    post_list=Post.objects.filter(category=cate).order_by("-created_time")
-    return render(request,"blog/index.html",context={"post_list":post_list})
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by("-created_time")
+    return render(request, "blog/index.html", context={"post_list": post_list})
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ltg=#ewr^zeclnkai=u=z=i9+my-#%g_0-k@s5+btj-(zf-n&5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["106.13.197.4","localhost", ".panhaitao.site"]
 
 # Application definition
 
@@ -117,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
